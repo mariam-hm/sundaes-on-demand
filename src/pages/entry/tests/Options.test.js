67e06a7084a6f9c80d9ed6cf19a7f852bf5a8bbb
Mariam Hammoud <hamm.mariam@gmail.com>
2022-10-10 20:49:03 -0400

    Added Scoops and Toppings and server tests

---
+++ src/pages/entry/tests/Options.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+
+import Options from '../Options';
+
+test('Displays image for each scoop from server', async () => {
+    render(<Options optionType="scoops" />);
+
+    // Find images
+    const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
+    expect(scoopImages).toHaveLength(2);
+
+    // Confirm alt text of images
+    const altText = scoopImages.map((element) => element.alt);
+    expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('Displays image for each topping from server', async () => {
+    render(<Options optionType="toppings" />);
+
+    const toppingImages = await screen.findAllByRole('img', { name: /topping$/i });
+    expect(toppingImages).toHaveLength(3);
+
+    const altText = toppingImages.map((element) => element.alt);
+    expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping']);
+});
